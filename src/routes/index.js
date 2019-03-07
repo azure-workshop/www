03b8684b1e5da98420c0896f09a3b2406f05b1b6
Anton Boyko <me@boykoant.pro>
2019-03-07 23:14:22 +0200

    views and data migrated

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,32 @@
+var fs = require('fs');
+
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get("/speakers", function(request, response){
+  var content = fs.readFileSync("./data/speakers.json", "utf8");
+  var speakers = JSON.parse(content);
+  response.render("speakers", { speakers:speakers });
+});
+
+router.get("/partners", function(request, response){
+  var content = fs.readFileSync("./data/partners.json", "utf8");
+  var partners = JSON.parse(content);
+  response.render("partners", { partners:partners });
+});
+
+router.get("/registration", function(request, response){
+  response.render("registration");
+});
+
+router.get("/agenda", function(request, response){
+  var content = fs.readFileSync("./data/agenda.json", "utf8");
+  var agenda = JSON.parse(content);
+  response.render("agenda", { agenda:agenda });
 });
 
 module.exports = router;
