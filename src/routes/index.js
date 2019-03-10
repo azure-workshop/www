9d3e404cbf64674f49f02f9a79c93a403637b7b5
Helen Boyko <boyko.helen@gmail.com>
2019-03-10 19:51:00 +0200

    basic content and layout added

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,19 +4,19 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Global Azure Bootcamp 2019 in Kyiv' });
 });
 
 router.get("/speakers", function(request, response){
   var content = fs.readFileSync("./data/speakers.json", "utf8");
   var speakers = JSON.parse(content);
-  response.render("speakers", { speakers:speakers });
+  response.render("speakers", { speakers:speakers, title: "Speakers of Global Azure Bootcamp 2019 in Kyiv" });
 });
 
 router.get("/partners", function(request, response){
   var content = fs.readFileSync("./data/partners.json", "utf8");
   var partners = JSON.parse(content);
-  response.render("partners", { partners:partners });
+  response.render("partners", { partners:partners, title: "Partners of Global Azure Bootcamp 2019 in Kyiv"  });
 });
 
 router.get("/registration", function(request, response){
@@ -26,7 +26,7 @@ router.get("/registration", function(request, response){
 router.get("/agenda", function(request, response){
   var content = fs.readFileSync("./data/agenda.json", "utf8");
   var agenda = JSON.parse(content);
-  response.render("agenda", { agenda:agenda });
+  response.render("agenda", { agenda:agenda, title: "Agenda of Global Azure Bootcamp 2019 in Kyiv"  });
 });
 
 module.exports = router;
