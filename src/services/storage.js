77f3146bcb1d2aad969acb115b7d6c2454bc6656
Anton Boyko <me@boykoant.pro>
2019-03-12 23:34:06 +0200

    storage service refactoring started

--- src/services/storage.js
+++ src/services/storage.js
@@ -30,6 +30,47 @@ service.init = function () {
     ]);
 };
 
+service.addAttendee = function(email, firstName, lastName, company, jobTitle) {
+    // var attendee = {
+    //     PartitionKey: { '_': '2019-04' },
+    //     RowKey: { '_': request.body.contactEmail.toLowerCase() },
+    //     FirstName: { '_': request.body.firstName },
+    //     LastName: { '_': request.body.lastName },
+    //     JobTitle: { '_': request.body.jobTitle },
+    //     Company: { '_': request.body.company },
+    //     isConfirmed: { '_': false, '$': 'Edm.Boolean' }
+    //   };
+
+    //TODO: check if user with this email is registered, if yes - do nothing and resolve promise
+    //TODO: insert new attendee to registrations table and resolve promise
+
+    return Promise.resolve();
+}
+
+service.addConfirmation = function(email, uniqueId) {
+    // var confirmation = {
+    //     PartitionKey: { '_': '2019-04' },
+    //     RowKey: { '_': mail.id },
+    //     Email: { '_': request.body.contactEmail.toLowerCase() },
+    // };
+
+    //TODO: insert new confirmation to confirmations table and resolve promise
+
+    return Promise.resolve();
+}
+
+service.confirmAttendee = function(email) {
+    //TODO: retreive attendee by email
+    //TODO: set is confirmed to true
+    //TODO: update attendee and resolve promise
+}
+
+service.findAttendeeEmail = function(uniqueId) {
+    //TODO: retreive entity from confirmation table by unique id
+    //TODO: if entity is empty, resolve promise with null value, otherwise resolve it with email value
+}
+
+//TODO: this method should be private
 service.insert = function (tableName, entity) {
     tableSvc.insertEntity(tableName, entity, function (error, result, response) {
         if (!error) {
@@ -39,6 +80,7 @@ service.insert = function (tableName, entity) {
     });
 };
 
+//TODO: this method should be private
 service.retrieve = function (tableName, PartitionKey, RowKey, callback) {
     tableSvc.retrieveEntity(tableName, PartitionKey, RowKey, function (error, result, response) {
         if (!error) {
@@ -50,6 +92,7 @@ service.retrieve = function (tableName, PartitionKey, RowKey, callback) {
     });
 };
 
+//TODO: this method should be private
 service.replace = function (tableName, name) {
     tableSvc.replaceEntity(tableName, name, function (error, result, response) {
 
